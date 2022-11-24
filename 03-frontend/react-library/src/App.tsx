23cc7b3d891471a1763e65bb5c41012b6c5d56a8
Rume7 <electengrhume@gmail.com>
2022-11-24 06:29:55 +0100

    Added the Heros' component.

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import { Carousel } from './layouts/HomePage/Carousel';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Heros } from './layouts/HomePage/Heros';
+import { LibraryServices } from './layouts/HomePage/LibraryServices';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <ExploreTopBooks />
-      <Carousel />
+      <Navbar/>
+      <ExploreTopBooks/>
+      <Carousel/>
+      <Heros/>
+      <LibraryServices/>
     </div>
   );
 }
