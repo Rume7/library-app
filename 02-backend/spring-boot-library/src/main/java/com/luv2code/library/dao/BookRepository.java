da42f96d9fede16dde285b598451983b647da70c
Rume7 <electengrhume@gmail.com>
2022-11-26 10:49:57 +0100

    Created the book dao

--- 02-backend/spring-boot-library/src/main/java/com/luv2code/library/dao/BookRepository.java
+++ 02-backend/spring-boot-library/src/main/java/com/luv2code/library/dao/BookRepository.java
@@ -1,2 +1,9 @@
-package com.luv2code.library.dao;public class BookRepository {
+package com.luv2code.library.dao;
+
+
+import com.luv2code.library.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
 }
